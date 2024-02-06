69433a933b3c0b4dd634aef97bf93f3da051a6a8
Josh Taylor <joshualiamtaylor@gmail.com>
2024-02-06 14:10:00 +1100

    Added cta on landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,6 +4,8 @@ import Featured from "../components/LandingPage/Featured"
 import Recent from "../components/LandingPage/Recent"
 import Categories from "../components/LandingPage/Categories"
 import useScrollToTop from "../hooks/useScrollToTop"
+import { Link } from "react-router-dom"
+import { FaCirclePlus, FaCircleArrowRight } from "react-icons/fa6"
 
 const Landing = ({ tools, tags }) => {
   const scrollToTop = useScrollToTop()
@@ -19,10 +21,27 @@ const Landing = ({ tools, tags }) => {
           <h1 className="mb-6 text-4xl font-semibold leading-tight tracking-tight md:text-6xl">
             Resources for your web toolkit
           </h1>
-          <p>
+          <p className="mb-6">
             Every craftsman needs a tool belt, so here's one for the web
             developers & designers.
           </p>
+          <div className="flex gap-4">
+            <Link
+              to={"tools"}
+              className="flex w-full items-center justify-center gap-x-2 rounded-md bg-primary p-2 font-medium tracking-wide text-white sm:w-[210px] sm:gap-x-4 sm:px-6 sm:py-2 sm:text-lg sm:transition-all sm:hover:bg-blue-700"
+            >
+              Browse Tools
+              <FaCircleArrowRight />
+            </Link>
+
+            <Link
+              to={"suggest"}
+              className="flex w-full items-center justify-center gap-x-2 rounded-md p-2 font-medium tracking-wide text-primary ring-[3px] ring-inset ring-primary sm:w-[210px] sm:gap-x-4 sm:px-6 sm:py-2 sm:text-lg sm:transition-all sm:hover:bg-primary  sm:hover:text-white "
+            >
+              Suggest
+              <FaCirclePlus />
+            </Link>
+          </div>
         </section>
         <section className="w-full md:px-12 lg:w-1/2">
           <AnimatedTags tags={tags} rows={rows} />
